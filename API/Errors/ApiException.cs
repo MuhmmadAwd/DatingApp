36878a0f602d67372c60977111ca11abf90b8254
mohamad awad <noza25588@gmail.com>
2022-06-05 09:44:10 +0300

    Exception handling middleware

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,25 @@
+﻿namespace API.Errors
+{
+    public class ApiException
+    {
+        private string v;
+
+        public ApiException(int statusCode, string v)
+        {
+            StatusCode = statusCode;
+            this.v = v;
+        }
+
+        public ApiException(int statusCode, string message, string detailes)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Detailes = detailes;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Detailes { get; set; }
+    }
+
+}
