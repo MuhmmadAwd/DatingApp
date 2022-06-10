c76c76fc0536c40c456022018801a9010b584dcd
mohamad awad <noza25588@gmail.com>
2022-06-10 14:52:53 +0300

    fix issues in the dataContext and databse

---
+++ API/Data/Migrations/20220610065539_updateKnownAs.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class updateKnownAs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "KnwnAs",
+                table: "Users",
+                newName: "KnownAs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "KnownAs",
+                table: "Users",
+                newName: "KnwnAs");
+        }
+    }
+}
