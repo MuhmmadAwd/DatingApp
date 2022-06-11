992c4cacf5807e329db698e65c51b79011a1b400
mohamad awad <noza25588@gmail.com>
2022-06-11 16:03:14 +0300

    Configuring cloudinary

---
+++ API/Helpers/ClouldinarySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Helpers
+{
+    public class ClouldinarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
