2a2c07509cee6ed20269a02d0d96b32b388eae54
mohamad awad <noza25588@gmail.com>
2022-06-15 08:44:41 +0300

    Updating the API register method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using API.DTOs;
 using API.Enities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
@@ -14,23 +15,27 @@ namespace API.Controllers
         private readonly DataContext context;
 
         public ITokenService tokenService;
+        private readonly IMapper mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService)
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
             this.context = context;
             this.tokenService = tokenService;
+            this.mapper = mapper;
         }
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+            var user = this.mapper.Map<AppUser>(registerDto);
+
             using var hmac = new HMACSHA512();
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
+
+
             this.context.Users.Add(user);
             await this.context.SaveChangesAsync();
 
@@ -38,6 +43,7 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = this.tokenService.CreateToken(user),
+                KnownAs = user.KnownAs,
             };
         }
 
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,6 +7,18 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+
+        [Required]
         [MinLength(6)]
         public string Password { get; set; }
     }
